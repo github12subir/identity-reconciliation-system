8329c586a1d19ce474a9b81114196fcef67e2aed
Subir Mondal <subir.m@stylopay.com>
2026-03-10 00:50:22 +0530

    Setup Express server and application entry point

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import identifyRoutes from "./routes/identifyRoutes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/identify", identifyRoutes);
+
+export default app;
