c5e9f677163d68ae6d886fe005e4aef3f4b4c708
Subir Mondal <subir.m@stylopay.com>
2026-03-11 00:18:52 +0530

    add root route

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.json());
 
+// root route
+app.get("/", (req, res) => {
+  res.send("Identity Reconciliation API running 🚀");
+});
+
 app.use("/identify", identifyRoutes);
 
 export default app;
